31fa37ece581c9f3e15991dbf4e343f17ba45248
uzin4916 <89904583+uUZINN@users.noreply.github.com>
2023-10-13 18:22:19 +0900

    2023.10.13

---
+++ php2/include/footer.php
@@ -0,0 +1,9 @@
+<footer id="footer" role="contentinfo">
+    <div class="footer__inner container btStyle">
+        <div>
+            Copyright 2023 uuzinn
+        </div>
+        <div>blog by uzin</div>
+    </div>
+</footer>
+<!-- //footer -->
